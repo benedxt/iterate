48d8d3f1f7285a02c0e821a9d31bbec680899115
benedxt <jike.exe@gmail.com>
2026-01-30 16:52:48 +0100

    bit better.

--- main.go
+++ main.go
@@ -4,39 +4,41 @@ import (
 	"math"
 )
 
-func GetData() (float64, float64, int) {
-	var initializer float64 
-	fmt.Print("initializer = ")
-	fmt.Scanln(&initializer)
-
-	var multiplier float64 
-	fmt.Print("multiplier = ")
-	fmt.Scanln(&multiplier)
-
-	var iterations int
-	fmt.Print("max rolls = ")
-	fmt.Scanln(&iterations)
-
-	return initializer, multiplier, iterations
-}
-
-func Iterate(in, mu float64, it int) []map[int]float64 { // may also return float64
-	var Ws []map[int]float64
-
-	for i := range it {
-		in = math.Floor(in * mu)
-		Ws = append(Ws, map[int]float64 {
-			i+1: in,
-		})
-	}
-	// return Ws[len(Ws)-1][len(Ws)] //value of last key of slice.
-	return Ws
-}
-
 func main() {
-	amount, odd, iterations := GetData()
-	Ws := Iterate(amount, odd, iterations)
-	fmt.Println(Ws)
+	initializer, multiplier, iterations := GetData()
+	ms := Iterate(initializer, multiplier, iterations)
+	current := ms[len(ms)-1][iterations]
+	fmt.Println(current) 
+
+	// find the iteration value at a specific index.
+	fmt.Print("find value at index: ")
+	var idx int
+	fmt.Scan(&idx)
+	favIdx := ms[idx-1]
+	fmt.Printf("value at index %v: %v\n", idx, favIdx[idx])
+
 }
  
 
+func GetData() (float64, float64, int) {
+	var initializer, multiplier float64 
+	fmt.Print("initializer: ")
+	fmt.Scanln(&initializer)
+	fmt.Print("multiplier: ")
+	fmt.Scanln(&multiplier)
+	var iterations int
+	fmt.Print("iterations: ")
+	fmt.Scanln(&iterations)
+	return initializer, multiplier, iterations
+}
+
+func Iterate(init, mtpl float64, itxn int) []map[int]float64 {
+	var ms []map[int]float64
+	for i := range itxn {
+		init = math.Floor(init * mtpl)
+		ms = append(ms, map[int]float64 {
+			i+1: init,
+		})
+	}
+	return ms
+}
